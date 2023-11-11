c378e6261bb005a63554fc9d35608317ec184056
Vaporgabe <gabestark01@gmail.com>
2023-11-11 03:36:12 -0600

    Added GSI

--- index.js
+++ index.js
@@ -119,11 +119,61 @@ function writeVCFGSync(path, obj) {
     fs.writeFileSync(path, vcfgWriteString(obj, 0));
 }
 
+class GSI {
+    constructor(options) {
+        this.name = "Gamestate Integration Config File";
+        this.uri = null;
+        this.timeout = null;
+        this.buffer = null;
+        this.throttle = null;
+        this.heartbeat = null;
+        this.auth = null;
+        this.data = [];
+
+        if(options.name) this.name = options.name;
+        if(options.uri) this.uri = options.uri;
+        if(options.timeout) this.timeout = options.timeout;
+        if(options.buffer) this.buffer = options.buffer;
+        if(options.throttle) this.throttle = options.throttle;
+        if(options.heartbeat) this.heartbeat = options.heartbeat;
+        if(options.auth) this.auth = options.auth;
+        if(options.data) this.data = options.data;
+    }
+
+    addData(data) {
+        this.data.push(data);
+    }
+
+    removeData(data) {
+        this.data.splice(this.data.indexOf(data), 1);
+    }
+
+    write(path) {
+        let obj = {};
+        if(this.uri || this.timeout || this.buffer || this.throttle || this.heartbeat) obj[this.name] = {};
+        if(this.uri) obj[this.name]["uri"] = this.uri;
+        if(this.timeout) obj[this.name]["timeout"] = this.timeout;
+        if(this.buffer) obj[this.name]["buffer"] = this.buffer;
+        if(this.throttle) obj[this.name]["throttle"] = this.throttle;
+        if(this.heartbeat) obj[this.name]["heartbeat"] = this.heartbeat;
+        if(this.auth) obj[this.name]["auth"] = this.auth;
+        if(this.data.length > 0) {
+            obj[this.name]["data"] = {};
+            for(let i = 0; i < this.data.length; i++) {
+                obj[this.name]["data"][this.data[i]] = 1;
+            }
+        }
+
+        writeVCFG(path, obj);
+    }
+}
+
 module.exports = {
     readVCFG,
     readVCFGSync,
     vcfgWriteString,
     writeVCFG,
     writeVCFGSync,
-    cmd
+    cmd,
+    GSI
 };
